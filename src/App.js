cd6821abd6240153cca89cfb4f8cf16046e718bd
YousifAlkhoury <yousif.alkhoury@gmail.com>
2020-10-03 16:13:23 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {GoogleMap, Marker, useLoadScript, InfoWindow} from '@react-google-maps/api'
+import mapStyles from './mapStyles.js'
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true
+}
+
 function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+    libraries
+  });
+
+  if(loadError) return "Error loading maps"
+  if(!isLoaded) return "Loading Maps";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Find mental health support available in your area using this interactive map.</h1>
+      <GoogleMap
+        mapContainerStyle = {mapContainerStyle} zoom={3} center = {{lat: 25, lng: 0}} options = {options}>
+
+      </GoogleMap>
     </div>
-  );
+  )
 }
 
 export default App;
